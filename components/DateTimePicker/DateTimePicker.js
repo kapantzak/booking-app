c2b609430418f0c6176573459b2fe10d5f9938f2
John Kapantzakis <kapantzak@gmail.com>
2022-06-15 10:34:04 +0300

    Display current timeZone and time

--- components/DateTimePicker/DateTimePicker.js
+++ components/DateTimePicker/DateTimePicker.js
@@ -2,9 +2,19 @@ import PropTypes from "prop-types";
 import Calendar from "@/components/Calendar";
 
 const DateTimePicker = ({ date, locales, sundayFirst }) => {
+  const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
+  const time = Intl.DateTimeFormat("el", {
+    hour: "numeric",
+    minute: "numeric",
+    timeZone,
+  }).format(new Date());
+
   return (
     <div>
       <Calendar date={date} locales={locales} sundayFirst={sundayFirst} />
+      <div>
+        Time zone: {timeZone} ({time})
+      </div>
     </div>
   );
 };
