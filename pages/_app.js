c5400f61d3b412c16d763f8feef8a0df9887232c
John Kapantzakis <kapantzak@gmail.com>
2022-07-15 00:35:37 +0300

    Add material-ui and x-date-pickers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline enableColorScheme />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import Head from "next/head";
+import TextField from "@mui/material/TextField";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 import DefaultLayout from "@/layouts/DefaultLayout";
 import { PROJECT_NAME } from "@/lib/constants";
 
 export default function Home() {
+  const [value, setValue] = useState(new Date());
+
   return (
     <DefaultLayout>
       <Head>
@@ -13,6 +20,17 @@ export default function Home() {
         />
       </Head>
       <h1 className="text-3xl">Booking app</h1>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <StaticDatePicker
+          displayStaticWrapperAs="desktop"
+          openTo="year"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
     </DefaultLayout>
   );
 }
