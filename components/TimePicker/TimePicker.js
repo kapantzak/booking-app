b450d3bbb22c0600c0a199f6bb722106a50c425f
John Kapantzakis <kapantzak@gmail.com>
2022-08-06 23:08:32 +0300

    Show warning if no slots are available

--- components/TimePicker/TimePicker.js
+++ components/TimePicker/TimePicker.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Alert } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { getSlots, getTimeElements } from "@/lib/timeHelpers";
 
@@ -37,20 +37,26 @@ const TimePicker = ({ slotDurationInMinutes, onTimeSelect }) => {
         },
       }}
     >
-      {slots.map(([hours, minutes]) => {
-        const time = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+      {slots.length > 0 &&
+        slots.map(([hours, minutes]) => {
+          const time = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
 
-        return (
-          <Button
-            sx={{ m: 1 }}
-            key={`${hours}-${minutes}`}
-            variant="outlined"
-            onClick={() => onTimeSelect([hours, minutes])}
-          >
-            {time}
-          </Button>
-        );
-      })}
+          return (
+            <Button
+              sx={{ m: 1 }}
+              key={`${hours}-${minutes}`}
+              variant="outlined"
+              onClick={() => onTimeSelect([hours, minutes])}
+            >
+              {time}
+            </Button>
+          );
+        })}
+      {slots.length === 0 && (
+        <Alert severity="warning">
+          No available slots! Please select another date.
+        </Alert>
+      )}
     </Box>
   );
 };
